9083314f888f6b9ec6f323cfe02a1308ded65363
piyushgarg362-commits <piyushgarg362@gmail.com>
2026-08-28 18:25:39 -0400

    Create GreetingService.java

---
+++ GreetingService.java
@@ -0,0 +1,12 @@
+package com.example.app;
+
+public class GreetingService {
+
+    public String getGreeting() {
+        return "Hello from Java Maven CI/CD!";
+    }
+
+    public String getApplicationName() {
+        return "Java Maven Demo Application";
+    }
+}
